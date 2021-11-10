9c054b83d51e999fa80d0a649e32f4de61f8a0c0
Exley <exley.roux@gmail.com>
2021-11-11 00:52:57 +0200

    Initial Commit

---
+++ PhotoShack/PhotoShack/src/test/java/xyz/ps/PhotoShack/PhotoShackApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ps.PhotoShack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoShackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
